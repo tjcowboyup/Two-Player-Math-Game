5b08f263e792821925f34003fb0c5d5e5a441259
Tom W <tjcowboyup@users.noreply.github.com>
2016-01-07 18:56:35 -0800

    Update logic.rb
    class
    random operation

--- logic.rb
+++ logic.rb
@@ -1,5 +1,35 @@
+require "./player"
+
 #generate random numbers between 1 & 20 for questions
 def random_numbers
-  @n1 = (1 + rand(19))
-  @n2 = (1 + rand(19))
+  @n1 = 1 + rand(20)
+  @n2 = 1 + rand(20)
+end
+
+def random_operator
+  operation = ['+', '-', '*', '/']
+  index = rand(4)
+  @operator = operation[index] 
+end
+
+def invalidGuessError
+  begin
+#  rescue NoMethodError => e
+#    puts "You can't do that!"
+    if @player_ans.is_a? !Integer
+      raise InvalidGuessError, "Not an answer in integer"
+    end
+  rescue InvalidGuessError => e
+    puts "You have to enter an integer for answer"
+  end
+end
+
+def invalidname
+  begin
+    if Player.name == "" || nil
+      raise InvalidNameError, "No name entered"
+    end
+  rescue InvalidNameError => e
+    puts "You have to enter your name"
+  end
 end
